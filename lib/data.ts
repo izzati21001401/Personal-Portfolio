66384002bd71707acfdaa4d3a2044e61c543ff74
izzati21001401 <117832847+izzati21001401@users.noreply.github.com>
2024-01-30 11:46:58 +0800

    wtv

--- lib/data.ts
+++ lib/data.ts
@@ -67,27 +67,6 @@ export const experiencesData = [
   },
 ] as const;
 
-export const projectsData = [
-  {
-    title: "Traventure",
-    description:
-      "My group designed and developed the app for Oh My Code 10.0 Competition in UTP under the theme Travel and Tourism",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-  },
-  {
-    title: "rmtDev",
-    description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-  },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-  },
-] as const;
-
 export const skillsData = [
   "HTML",
   "CSS",
